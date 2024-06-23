0264500b6f0d3377acfd1347b498fee60eb331a4
engageintellect <jc9361@gmail.com>
2024-06-23 14:58:44 -0700

    working guestbook

--- src/routes/guestbook/+page.server.ts
+++ src/routes/guestbook/+page.server.ts
@@ -1,69 +1,42 @@
 // +page.server.ts
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { error, fail } from '@sveltejs/kit';
+import { validateData } from '$lib/utils';
+import { createGuestBookPostSchema } from '$lib/schemas';
 
 // FETCH AND LOAD DATA
 export const load: PageServerLoad = async ({ locals }) => {
     if (!locals.pb.authStore.isValid) {
         throw redirect(303, '/auth/login');
     }
-    const posts = await fetchPostsData(locals);
-    const users = await fetchUsersData(locals);
-    return { posts, users };
-};
+    const res = await fetch('http://localhost:5173/api/getPosts');
+		const posts = await res.json();
 
-const fetchPostsData = async (locals: App.Locals): Promise<App.Post[]> => {
-    const records = await locals.pb.collection('posts').getFullList({
-        sort: '-created',
-    });
-    // Map RecordModel to Post
-    return records.map(record => ({
-        id: record.id,
-        author: record.author,
-        content: record.content,
-        collectionId: record.collectionId,
-        collectionName: record.collectionName,
-        created: record.created,
-        updated: record.updated
-    }));
-};
-
-const fetchUsersData = async (locals: App.Locals): Promise<App.User[]> => {
-    const records = await locals.pb.collection('users').getFullList({
-        fields: 'id,username',
-    });
-    // Map RecordModel to User
-    return records.map(record => ({
-        id: record.id,
-        username: record.username
-    }));
+    return { posts };
 };
 
 // ACTIONS
-import type { Actions } from './$types'
-import { error, fail, redirect } from '@sveltejs/kit';
-import { validateData } from '$lib/utils';
-import { createGuestBookPostSchema } from '$lib/schemas';
-
 export const actions: Actions = {
-	createPost: async ({ request, locals }) => {
-		const { formData, errors } = await validateData(await request.formData(), createGuestBookPostSchema);
+    createPost: async ({ request, locals }) => {
+        const { formData, errors } = await validateData(await request.formData(), createGuestBookPostSchema);
 
-		if (errors) {
-			return fail(400, {
-				data: formData,
-				errors: errors.fieldErrors
-			});
-		}
+        if (errors) {
+            return fail(400, {
+                data: formData,
+                errors: errors.fieldErrors
+            });
+        }
 
-		try {
-			await locals.pb.collection('posts').create(formData);
+        try {
+            await locals.pb.collection('posts').create(formData);
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		} catch (err: any) {
-			console.log('Error: ', 'error creating post: ' + err);
-			throw error(err.status, err.message);
-		}
+        } catch (err: any) {
+            console.log('Error: ', 'error creating post: ' + err);
+            throw error(err.status, err.message);
+        }
 
-		return { success: true };
-
-	}
+        return { success: true };
+    }
 };
--- src/routes/guestbook/+page.svelte
+++ src/routes/guestbook/+page.svelte
@@ -8,29 +8,25 @@
   import Icon from '@iconify/svelte'
 
   export let data
+  export let posts: any = []
 
-  let posts: App.Post[] = data.posts
-  let users: App.User[] = data.users
+  const fetchData = async () => {
+    const res = await fetch('/api/getPosts')
+    const posts = await res.json()
+    return posts
+  }
 
-  // Create a map of user IDs to usernames
-  let userMap = new Map<string, string>()
-  users.forEach((user) => {
-    userMap.set(user.id, user.username)
-  })
-
-  // Map posts to include the username
-  let postsWithUsernames = posts.map((post) => {
-    return {
-      ...post,
-      username: userMap.get(post.author) || 'Unknown',
-    }
-  })
+  let formElement: HTMLFormElement
 
   const handleSuccess = async (result: any) => {
     if (result.result.type === 'success') {
       await applyAction(result)
-      // TODO: There must be a better way to refresh the page
-      window.location.reload()
+      posts = await fetchData()
+      formElement.reset()
+      const textareaElement = formElement.querySelector('textarea')
+      if (textareaElement instanceof HTMLTextAreaElement) {
+        textareaElement.focus()
+      }
     } else {
       await applyAction(result)
     }
@@ -44,6 +40,7 @@
 
   <div class="flex flex-col gap-5 my-2">
     <form
+      bind:this={formElement}
       action="?/createPost"
       method="POST"
       class="w-full"
@@ -78,9 +75,18 @@
     <div class="w-full">
       <div class="">
         <div class="text-3xl">POSTS</div>
-        <div in:slide={{ duration: 1000 }} class="flex flex-col gap-2">
-          {#if postsWithUsernames.length > 0}
-            {#each postsWithUsernames as post}
+        <div in:slide={{ duration: 500 }} class="flex flex-col gap-2">
+          {#if posts.length === 0}
+            {#each data.posts as post}
+              <Post
+                user={post.username}
+                postContent={post.content}
+                postDate={post.created}
+              />
+            {/each}
+          {/if}
+          {#if posts.length > 0}
+            {#each posts as post}
               <Post
                 user={post.username}
                 postContent={post.content}
@@ -88,7 +94,7 @@
               />
             {/each}
           {:else}
-            <div class="alert alert-warning">
+            <div class="alert">
               <div>No posts yet.</div>
             </div>
           {/if}
