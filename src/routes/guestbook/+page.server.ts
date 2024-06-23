3c272fac82dd6b7721efa89d616b944a0a242d42
engageintellect <jc9361@gmail.com>
2024-06-23 16:16:37 -0700

    cleaning up

--- src/routes/guestbook/+page.server.ts
+++ src/routes/guestbook/+page.server.ts
@@ -6,36 +6,40 @@ import { error, fail } from '@sveltejs/kit';
 import { validateData } from '$lib/utils';
 import { createGuestBookPostSchema } from '$lib/schemas';
 
-// FETCH AND LOAD DATA
+interface CustomError {
+    status: number;
+    message: string;
+}
+
+// LOAD FUNCTION
 export const load: PageServerLoad = async ({ locals }) => {
     if (!locals.pb.authStore.isValid) {
         throw redirect(303, '/auth/login');
     }
-	// GET POSTS
-	const posts = await locals.pb.collection('posts').getFullList({
-			sort: '-created',
-	});
-	// GET USERS
-	const users = await locals.pb.collection('users').getFullList({
-			fields: 'id,username',
-	});
-	// ADD AUTHORS USERNAME TO POST OBJECT
-	const stuff = posts.map(post => ({
-			id: post.id,
-			author: post.author,
-			content: post.content,
-			collectionId: post.collectionId,
-			collectionName: post.collectionName,
-			created: post.created,
-			updated: post.updated,
-			username: users.find(user => user.id === post.author)?.username
-	}));
+    // GET POSTS
+    const posts = await locals.pb.collection('posts').getFullList({
+        sort: '-created',
+    });
+    // GET USERS
+    const users = await locals.pb.collection('users').getFullList({
+        fields: 'id,username',
+    });
+    // ADD AUTHORS USERNAME TO POST OBJECT
+    const stuff = posts.map(post => ({
+        id: post.id,
+        author: post.author,
+        content: post.content,
+        collectionId: post.collectionId,
+        collectionName: post.collectionName,
+        created: post.created,
+        updated: post.updated,
+        username: users.find(user => user.id === post.author)?.username
+    }));
 
-	return {posts: stuff};
+    return { posts: stuff };
 }
 
-
-// ACTIONS
+// FORM ACTIONS
 export const actions: Actions = {
     createPost: async ({ request, locals }) => {
         const { formData, errors } = await validateData(await request.formData(), createGuestBookPostSchema);
@@ -46,12 +50,12 @@ export const actions: Actions = {
             });
         }
         try {
-					await locals.pb.collection('posts').create(formData);
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (err: any) {
-            console.log('Error: ', 'error creating post: ' + err);
-            throw error(err.status, err.message);
-        }				
-        return {success: true};
+            await locals.pb.collection('posts').create(formData);
+        } catch (err) {
+            const customError = err as CustomError;
+            console.log('Error: ', 'error creating post: ' + customError.message);
+            throw error(customError.status, customError.message);
+        }
+        return { success: true };
     }
 };
