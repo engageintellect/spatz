3c9457e7dc2d021b6523aa0c29f2fe1f8a49029a
engageintellect <jc9361@gmail.com>
2024-06-29 23:08:14 -0700

    nullish coellencesfor pocketbase url

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 import PocketBase from 'pocketbase'
 
 export function createInstance() {
-  return new PocketBase(PUBLIC_POCKETBASE_URL)
+  return new PocketBase(PUBLIC_POCKETBASE_URL || 'http://localhost:8090')
 }
 
 export const pb = createInstance()
